e07747cdc7d8aef6a2bfc35d4aea3e1ddf225ecd
Shivam Sharma <mail2shivam94@gmail.com>
2019-10-03 01:00:51 +0530

    Removed unnecessary console logs

--- public/js/app.js
+++ public/js/app.js
@@ -10,17 +10,13 @@ weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     message1.textContent = 'Loading...'
     message2.textContent = ''
-    console.log('testing', search.value)
     fetch('http://localhost:3000/weather?address='+ search.value)
     .then((res) => {
         res.json().then((data) => {
             if(data.error){
-                console.log(data.error)
                 message1.textContent = data.error
             }
             else{
-                console.log(data.location)
-                console.log(data.forecastData)
                 message1.textContent = data.location
                 message2.textContent = data.forecastData
             }
